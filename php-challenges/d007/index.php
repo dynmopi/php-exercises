27d5f1d0cbf3952b27328cc6d9aa9a4b8569fb81
Danrley Maranhão <maranhaodanrley@gmail.com>
2024-06-10 12:17:43 -0300

    php-ch

--- php-challenges/d007/index.php
+++ php-challenges/d007/index.php
@@ -6,6 +6,39 @@
     <title>Document</title>
 </head>
 <body>
-    <header></header>
+    <header>
+        <h1>Preencha o seu salário</h1>
+    </header>
+    <main>
+        <form action="<?php $_SERVER['PHP_SELF']; ?>" method="get">
+            <section>
+                <label for="inum">Salário R$: </label>
+                <input type="number" id="inum" name="num">
+                <input type="submit" value="Enviar">
+            </section>
+            <section>
+                <?php  
+                  $num = $_GET["num"] ?? 0;
+                  $salario_min = 1412;
+  
+                  echo "Considerando o salário mínimo de $salario_min";
+              
+                ?>
+            </section>
+        </form>
+        <div id="res">
+        <?php 
+                $qntd = $num / 1412;
+                $sobra = $num % 1412;
+
+                echo "<h1>Resultado final</h1>";
+
+                $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+
+                echo "<p>Quem recebe o salário de $num, recebe ". round($qntd) . " salários mínimos + " . numfmt_format_currency($padrao, $sobra, "BRL") . "</p>";
+            
+            ?>
+        </div>
+    </main>
 </body>
 </html>
