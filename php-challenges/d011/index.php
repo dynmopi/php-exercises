0940bf0bf01e57581aaf5a790376dd54f36b4183
Danrley Maranhão <maranhaodanrley@gmail.com>
2024-06-12 12:37:02 -0300

    php-ch

--- php-challenges/d011/index.php
+++ php-challenges/d011/index.php
@@ -22,26 +22,44 @@
 </head>
 <body>
     <main>
-        <form action="<?php $_SERVER['PHP_SELF'] ?>">
+        <form action="<?php $_SERVER['PHP_SELF'] ?>" method="get">
             <label for="ipreco">preço do produto: </label>
             <input type="number" id="ipreco" name="preco">
 
             <label for="iperc">percentual: </label>
-            <input type="range" name="perc" id="iperc" step="1">
-            <span id='ivalue'>50</span>
+            <input type="range" name="perc" id="iperc" step="1" oninput="valor()"> 
+            <!--oninput é um evento dom que chama o js-->
+            <!--dê uma olhada no script-->
+            <span id='ivalue'>?</span><strong>%</strong>
 
             <input type="submit" value="submit">
         </form>
         <div id="script">
             <?php 
-            
+                //local onde os dados são adquiridos no formulário
+                $preco = $_GET['preco'] ?? 0;
+                $percentual = $_GET['perc'] ?? 0;
+                //cálculo de porcentagem
+                $porcentagem_calculada = $preco*($percentual/100);
+                $aumento = $preco + $porcentagem_calculada;
+
+                //formatação da moeda brasileira
+                $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+        
+                
+                echo "<h2>resultado: </h2>";
+                echo "<p>o produto que custava " . numfmt_format_currency($padrao, $preco, "BRL") . ", com <strong>$percentual% de aumento</strong>, vai passar a custar <strong>" . numfmt_format_currency($padrao, $aumento, "BRL"). "</strong> a partir de agora.</p>";
             ?>
         </div>
     </main>
     <script>
-        var range = window.document.getElementById('iperc'); 
-        var value = window.document.getElementById('ivalue');
+        //a função já é chamada quando o site é adentrado
+        valor()
 
+        //funcão definida para alterar o value do span
+        function valor(){
+            ivalue.innerText = iperc.value;
+        }
         
 
     </script>
---
+++ php-challenges/d012/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Calculadora de tempo</h1>
+    </header>
+    <main>
+        <section>
+            <form action="<?= $_SERVER['PHP_SELF']?>" method="get">
+                <label for="iseg">qual é o total de segundos? </label>
+                <input type="number" id="iseg" name="seg">
+                <input type="submit" value="submit">
+                <?php 
+                    // 1min -> 60s
+                    // 1h -> 60min -> 3600s
+                    // 1d -> 24h -> 1400min -> 86400s
+                    // 1sem -> 7d -> 168h -> 10080min -> 604400s
+
+                    $seg = $_GET['seg'] ?? 0;
+
+                    $sem = $seg / 604800;
+                    $sem_res = $seg % 604800;
+
+                    $dias = $sem_res / 86400;
+                    $dias_res = $sem_res % 86400;
+
+                    $horas = $dias_res / 3600;
+                    $horas_res = $dias_res % 3600;
+
+                    $min = $horas_res / 60;
+                    $min_res = $horas_res % 60;
+
+                    $seg_finais = $min_res;
+
+                    echo "<h2>totalizando: </h2>";
+
+                    echo "<p>analisando o valor digitado, <strong>$seg</strong> segundos equivalem a um total de: </p>";
+                    echo "<ul><li>" . (int)($sem) . " semanas</li>";
+                    echo "<li>" . (int)($dias) . " dias</li>";
+                    echo "<li>" . (int)($horas) . " horas</li>";
+                    echo "<li>" . (int)($min). " minutos</li>";
+                    echo "<li>" . (int)($seg_finais). " segundos</li></ul>";
+
+
+                
+                
+                    ?>
+            </form>
+
+        </section>
+        <div id="res">
+
+
+
+        </div>
+    </main>
+</body>
+</html>
